144578c55720f9eb8fd399f102c6e84d605ece9b
GRMams <dimamsjja@gmail.com>
2020-03-04 15:30:52 +0100

    last summer commit

--- src/main/java/fr/houseofcode/dap/server/dma/google/GmailService.java
+++ src/main/java/fr/houseofcode/dap/server/dma/google/GmailService.java
@@ -58,7 +58,7 @@ public class GmailService {
         List<Label> labels = listResponse.getLabels();
         String message = " ";
         if (labels.isEmpty()) {
-            return "Vous n'avez pas de label";
+            return "Vous n'avez aucun label.";
         } else {
 
             for (Label label : labels) {
